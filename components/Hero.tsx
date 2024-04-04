7d6a8b5417e60b896c97c3ad51484040f5ccae44
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-04 16:29:13 +0200

    a

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -52,7 +52,7 @@ const Hero = () => {
           />
         </div>
       </div>
-      <div className="relative flex flex-1 items-start">
+      <div className="relative flex  mr-40 items-start">
         <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
           <div className="flex flex-col">
             <div className="flexBetween">
