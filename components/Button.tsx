2399702654c7de290994067f45dc4a1ba55d5b9d
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-04 12:02:47 +0200

    btn created

---
+++ components/Button.tsx
@@ -0,0 +1,12 @@
+type ButtonProps = {
+  type: "button" | "submit";
+  title: string;
+  icon?: string;
+  variant: "btn_dark_green";
+};
+
+const Button = ({ type, title, icon, variant }: ButtonProps) => {
+  return <button type={type}></button>;
+};
+
+export default Button;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
+import Button from "./Button";
 
 const Navbar = () => {
   return (
@@ -19,7 +20,14 @@ const Navbar = () => {
           </Link>
         ))}
       </ul>
-      <div className="lg:flexCenter hidden"></div>
+      <div className="lg:flexCenter hidden">
+        <Button
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
     </nav>
   );
 };
