ddeebebc53feea7fdf7b7738ab5e36b0c28c41ce
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-04 11:56:11 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,10 +4,7 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav
-      className="flexBetween max-container padding-container relative z-30 py-5"
-      style={{ margin: " 1rem 5rem" }}
-    >
+    <nav className="flexBetween max-container padding-container relative z-30 py-5 m5">
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
