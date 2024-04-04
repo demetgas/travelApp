8740914d8ee27462131778ccada888cba8358e36
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-04 16:18:01 +0200

    Update Camp.tsx

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -7,6 +7,7 @@ interface CampProps {
   subtitle: string;
   peopleJoined: string;
 }
+
 const CampSite = ({
   backgroundImage,
   title,
@@ -27,13 +28,21 @@ const CampSite = ({
             <p className="regular-14 text-white">{subtitle}</p>
           </div>
         </div>
+
         <div className="flexCenter gap-6">
           <span className="flex -space-x-4 overflow-hidden">
             {PEOPLE_URL.map((url) => (
-              <Image src={url} key={url} alt="person" width={52} height={52} />
+              <Image
+                className="inline-block h-10 w-10 rounded-full"
+                src={url}
+                key={url}
+                alt="person"
+                width={52}
+                height={52}
+              />
             ))}
           </span>
-          <p className="bold-16t md:bold-20 text-white">{peopleJoined}</p>
+          <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
         </div>
       </div>
     </div>
@@ -57,15 +66,25 @@ const Camp = () => {
           peopleJoined="50+ Joined"
         />
       </div>
-      <div className="flexEnd mt-10 px-6 lg:mt-60 lg:mr-6">
-        <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 w-full overflow-hidden rounded-3xl">
-          <h2 className="regular-24 md:regular-32 2xl:regu">
-            <strong>Lorem ipsum</strong> {""}
-            dolor sit amet consectetur adipisicing elit. Non quia ipsum aliquam
-            quam explicabo! Quod laboriosam, quo ad reprehenderit animi eum
-            deleniti distinctio, dicta repellat ipsa eaque voluptas aperiam.
-            Obcaecati.
+
+      <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+        <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+          <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
+            <strong>Feeling Lost</strong> And Not Knowing The Way?
           </h2>
+          <p className="regular-14 xl:regular-16 mt-5 text-white">
+            Starting from the anxiety of the climbers when visiting a new
+            climbing location, the possibility of getting lost is very large.
+            That's why we are here for those of you who want to start an
+            adventure
+          </p>
+          <Image
+            src="/quote.svg"
+            alt="camp-2"
+            width={186}
+            height={219}
+            className="camp-quote"
+          />
         </div>
       </div>
     </section>
