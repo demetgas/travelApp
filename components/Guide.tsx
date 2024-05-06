95a69fb3216096fbf1c64034b0ac6a490d970ddf
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-05-06 15:01:21 +0200

    Update Guide.tsx

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Image from "next/image";
 
 const Guide = () => {
-  return <div>Guide</div>;
+  return (
+    <section className="flexCenter flex-col">
+      <div className="border-2 border-red-500 padding-container max-container w-full pb-24 ">
+        <Image alt="" src="/camp.svg" width={50} height={50} />
+      </div>
+    </section>
+  );
 };
 
 export default Guide;
