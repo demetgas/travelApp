2ad3d0cc5108924366c03db34ff8ca3a07977fd6
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-05-06 16:17:59 +0200

    Update Guide.tsx

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -30,7 +30,11 @@ const Guide = () => {
           height={580}
           className="w-full object-cover object-center 2xl:rounded-5xl"
         />
-        <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20"></div>
+        <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+          <div className="flexBetween flex-col">
+            <div className="flex w-full flex-col"></div>
+          </div>
+        </div>
       </div>
     </section>
   );
