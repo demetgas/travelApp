eb2186a440c24a66c6f5a040bf33c483567786fe
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-05-06 16:14:52 +0200

    Update Guide.tsx

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -13,7 +13,7 @@ const Guide = () => {
           <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">
             Guide You to Easy Path{" "}
           </h2>
-          <p>
+          <p className="regular-16 text-gray-30 xl:max-w-[520px]">
             Only with the hilink application you will no longer get lost and get
             lost again, because we already support offline maps when there is no
             internet connection in the field. Invite your friends, relatives and
@@ -21,6 +21,15 @@ const Guide = () => {
             the top of the mountain
           </p>
         </div>
+      </div>
+      <div className="flexCenter max-container relative w-full border-2 border-blue-500 ">
+        <Image
+          alt=""
+          src="/boat.png"
+          width={1400}
+          height={580}
+          className="w-full object-cover object-center 2xl:rounded-5xl"
+        />
       </div>
     </section>
   );
