08a73916f8add5d7c46e18073d4ec95e76a7c835
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-05-06 15:03:53 +0200

    Update Guide.tsx

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -6,6 +6,10 @@ const Guide = () => {
     <section className="flexCenter flex-col">
       <div className="border-2 border-red-500 padding-container max-container w-full pb-24 ">
         <Image alt="" src="/camp.svg" width={50} height={50} />
+        <p className="regular-18 -mt-1 mb-3 text-green-300">
+          WE ARE HERE FOR YOU
+        </p>
+        <div className="flex flex-wrap justify-between gap-5 lg:gap-10"></div>
       </div>
     </section>
   );
